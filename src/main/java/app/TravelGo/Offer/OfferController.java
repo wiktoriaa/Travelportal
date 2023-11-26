2e7814b1862dd69a6fd7c832a10a54bdb6b7dac7
emiliapiesnikowska <emila.piesnikowska@gmail.com>
2023-11-26 16:32:11 +0100

    Add pics to business offers

--- src/main/java/app/TravelGo/Offer/OfferController.java
+++ src/main/java/app/TravelGo/Offer/OfferController.java
@@ -1,19 +1,20 @@
 package app.TravelGo.Offer;
 
+import app.TravelGo.File.FileService;
 import app.TravelGo.Post.Post;
 import app.TravelGo.Post.PostService;
 import app.TravelGo.Post.PostType;
 import app.TravelGo.User.Auth.AuthService;
 import app.TravelGo.User.UserService;
-import app.TravelGo.dto.CreatePostRequest;
-
 import app.TravelGo.dto.GetPostResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.*;
 
@@ -24,25 +25,32 @@ public class OfferController {
     private final PostService postService;
     private final AuthService authService;
     private final UserService userService;
+    private final FileService fileService;
 
     @Autowired
     public OfferController(PostService postService, AuthService authService,
-                           UserService userService) {
+                           UserService userService, FileService fileService) {
         this.postService = postService;
         this.authService = authService;
         this.userService = userService;
+        this.fileService = fileService;
     }
 
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
-    public @ResponseBody
-    ResponseEntity<Void> createPost(@RequestBody CreatePostRequest request, UriComponentsBuilder builder) {
+    public ResponseEntity<Void> createPost(
+            @RequestParam("title") String title,
+            @RequestParam("content") String content,
+            @RequestParam("about") String about,
+            @RequestParam(value = "images", required = false) List<MultipartFile> images,
+            UriComponentsBuilder builder) throws IOException {
+
         Post post = Post.builder()
-                .title(request.getTitle())
-                .content(request.getContent())
+                .title(title)
+                .content(content)
                 .likes(0L)
-                .username(this.authService.getCurrentUser().getUsername())
-                .about(request.getAbout())
+                .username(authService.getCurrentUser().getUsername())
+                .about(about)
                 .updatedAt(LocalDateTime.now())
                 .createdAt(LocalDateTime.now())
                 .type(PostType.OFFER)
@@ -51,9 +59,16 @@ public class OfferController {
 
         post = postService.createPost(post);
 
+        if (images != null) {
+            for (MultipartFile image : images) {
+                fileService.uploadFeaturePostImage(image, post.getId());
+            }
+        }
+
         return ResponseEntity.created(builder.pathSegment("api", "posts", "{id}")
                 .buildAndExpand(post.getId()).toUri()).build();
     }
+
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
@@ -77,6 +92,7 @@ public class OfferController {
                     .about(post.getAbout())
                     .createdAt(post.getCreatedAt())
                     .likes(post.getLikes())
+                    .imagesDir("/api/files/posts/" + post.getId())
                     .build();
             postResponses.add(postResponse);
         }
@@ -92,17 +108,21 @@ public class OfferController {
 
         if (response.isPresent()) {
             Post post = response.get();
-            GetPostResponse postResponse = GetPostResponse.builder()
-                    .id(post.getId())
-                    .title(post.getTitle())
-                    .content(post.getContent())
-                    .username(post.getUsername())
-                    .userID(userService.getUserByUsername(post.getUsername()).get().getId())
-                    .about(post.getAbout())
-                    .createdAt(post.getCreatedAt())
-                    .likes(post.getLikes())
-                    .build();
-            return ResponseEntity.ok(postResponse);
+            if (post.getType() == PostType.OFFER) {
+                GetPostResponse postResponse = GetPostResponse.builder()
+                        .id(post.getId())
+                        .title(post.getTitle())
+                        .content(post.getContent())
+                        .username(post.getUsername())
+                        .userID(userService.getUserByUsername(post.getUsername()).get().getId())
+                        .about(post.getAbout())
+                        .createdAt(post.getCreatedAt())
+                        .updatedAt(post.getUpdatedAt())
+                        .likes(post.getLikes())
+                        .imagesDir("/api/files/posts/" + post.getId())
+                        .build();
+                return ResponseEntity.ok(postResponse);
+            } else return ResponseEntity.badRequest().build();
         }
         return ResponseEntity.notFound().build();
     }
